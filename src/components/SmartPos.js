cf16343438d52f29baa19c6667ff02185cec0322
Benjamin-expertDev <benjaminmwambura@gmail.com>
2023-08-07 11:51:54 +0300

    commit

--- src/components/SmartPos.js
+++ src/components/SmartPos.js
@@ -4,12 +4,10 @@ function SmartPos() {
   const [showDialog, setShowDialog] = useState(false);
 
   const handleForgotPasswordClick = () => {
-    
     setShowDialog(true);
   };
 
   const handleCloseDialog = () => {
-    
     setShowDialog(false);
   };
 
@@ -23,13 +21,13 @@ function SmartPos() {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
                 stroke="currentColor"
                 className="w-6 h-6"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={1.5}
                   d="M21 12a2.25 2.25 0 00-2.25-2.25H15a3 3 0 11-6 0H5.25A2.25 2.25 0 003 12m18 0v6a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 9m18 0V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v3"
                 />
               </svg>
@@ -51,13 +49,13 @@ function SmartPos() {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
                 stroke="currentColor"
                 className="w-6 h-6"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={1.5}
                   d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
                 />
               </svg>
@@ -75,13 +73,13 @@ function SmartPos() {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
                 stroke="currentColor"
                 className="w-6 h-6"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={1.5}
                   d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
                 />
               </svg>
@@ -110,12 +108,14 @@ function SmartPos() {
         </form>
       </main>
 
-      {/* Conditionally render the dialog based on state */}
+      {/* Dialog Box */}
       {showDialog && (
-        <div className="dialog">
-          <h3>Contact Benjamin</h3>
-          <p>Here is the contact information for Benjamin.</p>
-          <button onClick={handleCloseDialog}>Close</button>
+        <div style={{ position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, background: 'rgba(0, 0, 0, 0.5)', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <div style={{ background: '#fff', padding: '20px', borderRadius: '4px', boxShadow: '0 2px 8px rgba(0, 0, 0, 0.2)' }}>
+            <h3>Contact Benjamin</h3>
+            <p>+254748454577</p>
+            <button onClick={handleCloseDialog}>Open Contact</button>
+          </div>
         </div>
       )}
     </div>
